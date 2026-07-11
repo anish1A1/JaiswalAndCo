3f72273ded764943b2043decf13901e6d5076757
Anish Jaiswal <alloozpat87@gmail.com>
2026-07-11 23:20:24 +0545

    created some body of the project and also added shadcn

---
+++ components/Header.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 border-b bg-white">
+      <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
+        {/* Logo */}
+        <Link
+          href="/"
+          className="text-2xl font-bold tracking-tight text-red-600"
+        >
+          JS
+        </Link>
+
+        {/* Profile */}
+        <DropdownMenu>
+          <DropdownMenuTrigger className="outline-none cursor-pointer">
+              <Avatar className="h-10 w-10">
+                <AvatarFallback>JS</AvatarFallback>
+              </Avatar>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>Logout</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </header>
+  );
+}
