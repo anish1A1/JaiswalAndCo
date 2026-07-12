9198adef68f1b68c9c75645c19e78e85d99f7eb3
Anish Jaiswal <alloozpat87@gmail.com>
2026-07-12 07:18:24 +0545

    rearranged the page and header

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="bg-gray-50 min-h-full flex flex-col">
+      <body className="bg-gray-500 min-h-full flex flex-col">
       <Header />
         
         {children}</body>
--- components/Header.tsx
+++ components/Header.tsx
@@ -13,7 +13,7 @@ import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 border-b bg-white">
+    <header className="sticky top-0 z-200 border-b bg-white">
       <div className="mx-auto flex h-16 max-w-6xl items-center justify-between px-4">
         {/* Logo */}
         <Link
