8c9c6f968febae02453e4696c685c71b90535514
Anish Jaiswal <alloozpat87@gmail.com>
2026-09-02 10:11:28 +0545

    removed middleware

--- proxy.ts
+++ proxy.ts
@@ -1,16 +1,26 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import {jwtVerify} from 'jose'
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 
-export function proxy(request: NextRequest){
-    const session = request.cookies.get('admin_cookie')?.value
+const SECRET = new TextEncoder().encode(process.env.SESSION_SECRET);
 
-    if (request.nextUrl.pathname.startsWith('/admin') && session !== 'true') {
-        return NextResponse.redirect(new URL('/', request.url))
+export async function proxy(request: NextRequest) {
+    const sessionToken = request.cookies.get('session')?.value;
+
+    // 1. If no cookie exists, redirect to login page
+    if(!sessionToken){
+        return NextResponse.redirect(new URL('/rejoin', request.url));
     }
 
-    return NextResponse.next()
+    try {
+        // 2. Verify token validity at the Vercel Edge
+        await jwtVerify(sessionToken, SECRET);
+        return NextResponse.next();
+    } catch (error) {
+        return NextResponse.redirect(new URL('/rejoin', request.url));
+    }
 }
 
 export const config = {
-    matcher: '/admin/:path*'
+    matcher: [`/admin/:path*`], // Protects everything under these folders
 }
